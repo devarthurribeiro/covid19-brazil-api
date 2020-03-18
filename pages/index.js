1b92a3c4d3526e6f55d76dcf4edb5b06b9514fba
Arthur Ribeiro <devarthurribeiro@gmail.com>
2020-03-18 12:35:15 -0300

    🏠add home page project

--- pages/index.js
+++ pages/index.js
@@ -3,45 +3,45 @@ import Head from 'next/head'
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>COVID-19 Brazil API</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        📊 COVID-19 Brazil API
       </h1>
 
       <p className="description">
-        Get started by editing <code>pages/index.js</code>
+        Notificação de casos de doença pelo coronavírus 2019 (COVID-19) no Brasil.
       </p>
 
       <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
+        <a href="https://github.com/devarthurribeiro/covid19-brazil-api/blob/master/README.md" className="card">
+          <h3>Documentação &rarr;</h3>
+          <p>Veja a doc da api</p>
         </a>
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
+        <a href="https://coronavirus.saude.gov.br/" className="card">
+          <h3>Leia &rarr;</h3>
+          <p>Sobre o virus</p>
         </a>
 
         <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="http://plataforma.saude.gov.br/novocoronavirus/"
           className="card"
         >
-          <h3>Deploy &rarr;</h3>
+          <h3>Dados &rarr;</h3>
+          <p>Ministério da Saúde.</p>
+        </a>
+
+        <a
+          href="https://github.com/devarthurribeiro"
+          className="card"
+        >
+          <h3>Autor &rarr;</h3>
           <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
+            @devarthurribeiro.
           </p>
         </a>
       </div>
@@ -49,11 +49,11 @@ const Home = () => (
 
     <footer>
       <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        href="https://github.com/devarthurribeiro"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
+        Feito com 💙 @devarthurribeiro.
       </a>
     </footer>
 
