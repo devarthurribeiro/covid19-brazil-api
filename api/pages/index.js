c1917e957c3cc05be083e1e968a3570b70bec5c7
Arthur Ribeiro <devarthurribeiro@gmail.com>
2020-03-21 08:35:11 -0300

    add donate button in home page

--- api/pages/index.js
+++ api/pages/index.js
@@ -65,6 +65,16 @@ function Home (props) {
           />
           <br />
         </div>
+        <div className="flex-center">
+          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_donations" />
+            <input type="hidden" name="business" value="PWCXMS2YKHJ7A" />
+            <input type="hidden" name="currency_code" value="BRL" />
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+            <img alt="" border="0" src="https://www.paypal.com/en_BR/i/scr/pixel.gif" width="1" height="1" />
+          </form>
+        </div>
+        <br/>
       </main>
 
       <footer>
