d3c0fd52c72e7baa5f05b23a0f399703adb2a628
Arthur Ribeiro <devarthurribeiro@gmail.com>
2020-03-21 14:11:42 -0300

    fix country endpoint

--- api/pages/api/report/v1/[country].js
+++ api/pages/api/report/v1/[country].js
@@ -13,16 +13,16 @@ async function Country(request, response) {
     }query?f=json&where=Country_Region='${country}'&outFields=*&cacheHint=true`,
   );
   const jsonDataset = JSON.parse(dataset);
-
   if (jsonDataset.features.length > 0) {
+    const data = jsonDataset.features[0].attributes;
     response.json({
       data: {
-        country: country.Country_Region,
-        cases: country.Active,
-        confirmed: country.Confirmed,
-        deaths: country.Deaths,
-        recovered: country.Recovered,
-        updated_at: new Date(country.Last_Update),
+        country: data.Country_Region,
+        cases: data.Active,
+        confirmed: data.Confirmed,
+        deaths: data.Deaths,
+        recovered: data.Recovered,
+        updated_at: new Date(data.Last_Update),
       },
     });
   } else {
