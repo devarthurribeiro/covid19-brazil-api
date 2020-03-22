9b9f4f88224fd332b518573ea072b04c849693f6
Arthur Ribeiro <devarthurribeiro@gmail.com>
2020-03-22 06:01:53 -0300

    add sidenav componet

---
+++ api/components/SideNav.js
@@ -0,0 +1,77 @@
+const SideNav = () => (
+  <aside className="sidenav">
+    <nav className="menu">
+      <a href="/">
+        <i className="material-icons">home</i>
+      </a>
+      <a href="/status">
+        <i className="material-icons">insert_chart</i>
+      </a>
+      <a href="/">
+        <i className="material-icons">dashboard</i>
+      </a>
+      <a href="https://covid19-brazil-api-docs.now.sh/">
+        <i className="material-icons">book</i>
+      </a>
+      <a href="/">
+        <i className="material-icons">info</i>
+      </a>
+    </nav>
+
+    <style jsx>
+      {`
+      .sidenav {
+        z-index: 1;
+        height: 100vh;
+        padding: 0 8px;
+        transition: all .2s ease-in-out;
+        display: flex;
+        order: 1;
+        flex-flow: column;
+        position: sticky;
+        top: 0;
+        align-self: flex-start;
+        background-color: rgba(0, 0, 0, 0.2);
+      }
+
+      nav {
+        display: flex;
+        flex-flow: column nowrap;
+      }
+
+      nav a {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-shrink: 0;
+        padding: 14px;
+        border-radius: 50%;
+        background-color: hsla(0, 0%, 100%, 0.2);
+        color: hsla(0, 0%, 100%, 0.5);
+        fill: hsla(0, 0%, 100%, 0.5);
+        margin: 8px 0;
+        height: 52px;
+      }
+
+      .menu {
+        display: flex;
+        flex-flow: column nowrap;
+      }
+
+      .menu .item {
+        width: 56px;
+        height: 56px;
+        background-color: #202124;
+      }
+
+      @media (max-width: 600px) {
+        footer {
+          flex-direction: column;
+        }
+      }
+    `}
+    </style>
+  </aside>
+);
+
+export default SideNav;
