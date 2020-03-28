9889cdb844933108a6c18523a36de91a24481bf7
Arthur Ribeiro <devarthurribeiro@gmail.com>
2020-03-28 18:48:33 -0300

    🔥remove not used data map

--- crawler/helpres/dataMaps.js
+++ crawler/helpres/dataMaps.js
@@ -17,30 +17,12 @@ function getStateInfo(uf) {
 const toInt = (n) => parseInt(n.replace(/[^0-9]/g, ''), 10);
 
 const dataMaps = {
-  ms: (item) => ({
-    uid: item.uid || '',
-    uf: states.names[item.uid] || '',
-    state: states.codes[item.uid] || '',
-    cases: item.cases || 0,
-    deaths: item.deaths || 0,
-    suspects: item.suspects || 0,
-    refuses: item.refuses || 0,
-  }),
   od: (item) => ({
     ...getStateInfo(item.u),
     cases: item.c || 0,
     deaths: item.o || 0,
     suspects: item.s || 0,
     refuses: item.d || 0,
-  }),
-  arcgis: (item) => ({
-    uid: item.cod_uf || 0,
-    uf: states.codes[item.cod_uf] || '',
-    state: states.names[item.cod_uf] || '',
-    cases: item.casos_conf || 0,
-    deaths: item.obitos || 0,
-    suspects: item.casos_susp || 0,
-    refuses: item.casos_desc || 0,
   }),
   msSite: (item) => ({
     ...getStateInfo(item['1']),
