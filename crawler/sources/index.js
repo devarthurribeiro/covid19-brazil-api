87476eb80bc33d62570ba8388fd248fdcb688fd1
Arthur Ribeiro <devarthurribeiro@gmail.com>
2020-04-05 22:11:07 -0300

    create data map of covid.saude.gov.br

--- crawler/sources/index.js
+++ crawler/sources/index.js
@@ -1,4 +1,19 @@
+const parseData = require('../helpres/parserData');
+const { mergeReports } = require('../util');
+
 const sources = [
+  {
+    url: process.env.MS_URL,
+    headers: { 'x-parse-application-id': process.env.APPLICATION_ID },
+    formatBody: (body) => {
+      const report = parseData(body.results, undefined, 'msPlatform');
+      // eslint-disable-next-line global-require
+      const lastReport = require('../../data/ms/report.json');
+      const mergedReport = mergeReports(lastReport, report);
+      console.table(mergedReport);
+      return mergedReport;
+    },
+  },
 ];
 
 module.exports = sources;
